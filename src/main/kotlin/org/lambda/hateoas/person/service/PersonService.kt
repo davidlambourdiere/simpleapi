30b0ae667ba98fbcd6aa0db3ece44029d7057ae0
davidlambourdiere <46440836+davidlambourdiere@users.noreply.github.com>
2022-07-07 22:54:47 +0200

    replace PersonService interface by its only implementation

--- src/main/kotlin/org/lambda/hateoas/person/service/PersonService.kt
+++ src/main/kotlin/org/lambda/hateoas/person/service/PersonService.kt
@@ -1,13 +1,38 @@
 package org.lambda.hateoas.person.service
 
 import org.lambda.hateoas.person.model.dto.PersonDTO
+import org.lambda.hateoas.person.model.mapper.PersonMapper
+import org.lambda.hateoas.person.repository.PersonRepository
+import javax.enterprise.context.ApplicationScoped
+import javax.transaction.Transactional
+import javax.ws.rs.NotFoundException
 
-interface PersonService {
-    fun getPerson(id: Long): PersonDTO
+@ApplicationScoped
+class PersonService(
+        val personRepository: PersonRepository,
+        val personMapper: PersonMapper) {
 
-    fun createPerson(personDto: PersonDTO): PersonDTO
 
-    fun getAllPerson(): List<PersonDTO>
+    fun getPerson(id: Long): PersonDTO = personMapper.toPersonDto(findPersonById(id))
 
-    fun updatePerson(id: Long, personDto: PersonDTO): PersonDTO
+    @Transactional
+    fun createPerson(personDto: PersonDTO): PersonDTO {
+        return personMapper.toPerson(personDto).let {
+            personRepository.persist(it)
+            personMapper.toPersonDto(it)
+        }
+    }
+    fun getAllPerson(): List<PersonDTO> = personRepository.listAll().map { personMapper.toPersonDto(it) }
+
+    @Transactional
+    fun updatePerson(id: Long, personDto: PersonDTO): PersonDTO {
+        val person = findPersonById(id)
+        return personMapper.toExistingPerson(personDto, person).let {
+            personRepository.persist(it)
+            personMapper.toPersonDto(it)
+        }
+    }
+
+    fun findPersonById(id: Long) =
+            personRepository.findById(id) ?: throw NotFoundException("Couldn't find person with id %d".format(id))
 }
